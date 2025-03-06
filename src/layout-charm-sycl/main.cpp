f8ccabb6b51335c107d991d7de8738d9a4ef558f
byumak01 <byumak01@gmail.com>
2025-03-06 18:56:44 +0300

    layout-charm-sycl error-fix

--- src/layout-charm-sycl/main.cpp
+++ src/layout-charm-sycl/main.cpp
@@ -117,7 +117,7 @@ sycl::buffer<int, 1> outputBuffer(deviceResult, treeNumber);
       sycl::accessor<int, 1, sycl::access_mode::write> Result(outputBuffer, cgh);
 
       cgh.parallel_for(ndr, [=] (sycl::nd_item<3> const& item) {
-        int gid = item.get_global_id(2);  // Use the Z-dimension as the 1D index
+        int gid = (int)*item.get_global_linear_id();  // Use the Z-dimension as the 1D index
         int res = 0;
         for (int j = 0; j < treeSize; j++) {
             res += AppleTree[gid * treeSize + j];
