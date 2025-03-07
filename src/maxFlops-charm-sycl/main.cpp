11a2c42fbb198df1f117c6b201367fcfdb7d6048
byumak01 <byumak01@gmail.com>
2025-03-07 19:56:57 +0300

    maxFlop-charm-sycl test

--- src/maxFlops-charm-sycl/main.cpp
+++ src/maxFlops-charm-sycl/main.cpp
@@ -23,8 +23,8 @@ void test (sycl::queue &q, const int repeat, const int numFloats)
   //sycl::buffer<T, 1> hostMem_buffer(hostMem, numFloats);
   //sycl::buffer<T, 1> deviceMem_buffer(_deviceMem, numFloats);
 
-  sycl::buffer hostMem_buffer{hostMem};
-  sycl::buffer deviceMem_buffer{_deviceMem};
+  sycl::buffer hostMem_buffer(hostMem);
+  sycl::buffer deviceMem_buffer(_deviceMem);
 
   sycl::range<1> gws (numFloats);
   sycl::range<1> lws (BLOCK_SIZE);
